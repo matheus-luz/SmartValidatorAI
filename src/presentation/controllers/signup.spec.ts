994a1803034616452df48dfd26cad83f77ca327b
matheus-luz <luz.matheus11@gmail.com>
2025-04-03 17:47:14 -0300

    refactor: move EmailValidator creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -9,14 +9,28 @@ interface SutTypes {
     emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         isValid (email: string): boolean {
             return false;
         }
-    } 
-    const emailValidatorStub = new EmailValidatorStub()
+    }
+    return new EmailValidatorStub()
+}
+
+const makeEmailValidatorWithError = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        isValid (email: string): boolean {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+    const emailValidatorStub = makeEmailValidator()
     const sut = new SignUpController(emailValidatorStub);
     return {
         sut,
@@ -113,13 +127,7 @@ describe('SignUp Controller', () => {
     })
 
     test('Should return 500 if EmailValidator throws', () => {
-        class EmailValidatorStub implements EmailValidator {
-            // eslint-disable-next-line @typescript-eslint/no-unused-vars
-            isValid (email: string): boolean {
-                throw new Error()
-            }
-        }
-        const emailValidatorStub = new EmailValidatorStub()
+        const emailValidatorStub = makeEmailValidatorWithError()
         const sut = new SignUpController(emailValidatorStub);
         const httpRequest = {
             body: {
