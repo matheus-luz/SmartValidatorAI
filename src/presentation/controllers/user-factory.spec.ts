7eb835e8298b73eaf0ec0055991169b1e2739579
matheus-luz <luz.matheus11@gmail.com>
2025-06-10 16:59:38 -0300

    refactor: delete files signUp controller and insert user factory development

---
+++ src/presentation/controllers/user-factory.spec.ts
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { MissingParamError } from '../errors/missing-param-erros'
+import { EmailValidator } from '../protocols/email-validator'
+import { UserFactoryController } from './user-factory'
+
+interface SutTypes {
+    sut: UserFactoryController,
+    emailValidatorStub: EmailValidator
+}
+
+const makeSut = (): SutTypes => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid(email: string): boolean {
+            return true
+        }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new UserFactoryController(emailValidatorStub)
+    return {
+        sut, 
+        emailValidatorStub
+    }
+}
+
+describe('UserFactory Controller', () => {
+    test('Should return 440 if no email is provided', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                password: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    })
+
+    test('Should return 440 if no password is provided', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    })
+
+    test('Should return 440 if an invalid email is provided', () => {
+        const { sut, emailValidatorStub } = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        const httpRequest = {
+            body: {
+                email: 'invalid_email@mail.com',
+                password: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    })
+})
---
+++ src/presentation/controllers/user-factory.ts
@@ -0,0 +1,32 @@
+import { badRequest } from './../helpers/http-helpers';
+import { HttpRequest, HttpResponse } from '../protocols/http'
+import { MissingParamError } from '../errors/missing-param-erros'
+import { Controller } from '../protocols/controller';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../errors/invalid-param-error';
+
+export class UserFactoryController implements Controller {
+    private readonly emailValidator: EmailValidator
+
+    constructor (emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator
+    }
+
+    handle(httpRequest: HttpRequest): HttpResponse {
+        const requiredFields = ['email', 'password']
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingParamError(field))
+            }
+        }
+        const isValid = this.emailValidator.isValid(httpRequest.body.email)
+        if (!isValid) {
+            return badRequest(new InvalidParamError('email'))
+        }
+
+        return {
+            statusCode: 200,
+            body: ''
+        }
+    }
+}
