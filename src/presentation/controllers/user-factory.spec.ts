6b31296f77abf1bec8af81ae1bc97c7a0403f9fc
matheus-luz <luz.matheus11@gmail.com>
2025-06-28 16:20:51 -0300

    feat: ensure UserFactory controller returns 500 if EmailValidator throws

--- src/presentation/controllers/user-factory.spec.ts
+++ src/presentation/controllers/user-factory.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-erros'
+import { ServerError } from '../errors/server-error'
 import { EmailValidator } from '../protocols/email-validator'
 import { UserFactoryController } from './user-factory'
 
@@ -74,5 +75,24 @@ describe('UserFactory Controller', () => {
         }
         sut.handle(httpRequest)
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+    })
+
+    test('Should returns 500 if EmailValidar throws', () => {
+        class EmailValidatorStub implements EmailValidator {
+            isValid(email: string): boolean {
+                throw new Error()
+            }
+        }
+        const emailValidatorStub = new EmailValidatorStub()
+        const sut = new UserFactoryController(emailValidatorStub)
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password',
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
     })
 })
--- src/presentation/controllers/user-factory.ts
+++ src/presentation/controllers/user-factory.ts
@@ -4,6 +4,7 @@ import { MissingParamError } from '../errors/missing-param-erros'
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamError } from '../errors/invalid-param-error';
+import { ServerError } from '../errors/server-error';
 
 export class UserFactoryController implements Controller {
     private readonly emailValidator: EmailValidator
@@ -13,20 +14,28 @@ export class UserFactoryController implements Controller {
     }
 
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['email', 'password']
-        for (const field of requiredFields) {
-            if (!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field))
+        try {
+            const requiredFields = ['email', 'password']
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
-        }
-        const isValid = this.emailValidator.isValid(httpRequest.body.email)
-        if (!isValid) {
-            return badRequest(new InvalidParamError('email'))
-        }
+            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+            }
 
-        return {
-            statusCode: 200,
-            body: ''
+            return {
+                statusCode: 200,
+                body: ''
+            }
+        } catch (error) {
+            console.error(error);
+            return {
+                statusCode: 500,
+                body: new ServerError()
+            }
         }
     }
 }
